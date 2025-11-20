c7b5090a5d7f9e35989f3763b4f7e96308cccb09
kaiyfa <adamal.sall@gmail.com>
2025-11-20 01:04:02 +0000

    Merged main into develop and preserved local Docker DB configuration

--- aramco_maintenance/settings.py
+++ aramco_maintenance/settings.py
@@ -29,15 +29,20 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "dashboard",
+    # Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    
+    # Local apps
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
